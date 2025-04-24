4dcd33009eecfaa891bf2372169463d96333cb05
Cisco Ramon <aungkoman@gmail.com>
2025-04-24 19:48:29 +0630

    hotel listing api added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Hotel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('v1/hotels', function () {
+    $hotels = Hotel::all();
+    return response()->json($hotels);
 });
+
