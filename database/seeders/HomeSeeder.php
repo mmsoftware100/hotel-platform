31a317872a5e9297c6808b78b810c1abbc2e8dca
Cisco Ramon <aungkoman@gmail.com>
2025-05-31 00:55:23 +0630

    delayed and media updated

--- database/seeders/HomeSeeder.php
+++ database/seeders/HomeSeeder.php
@@ -16,8 +16,8 @@ class HomeSeeder extends Seeder
         Home::create([
             'name' => 'Welcome to Golden Land',
             'slug' => 'welcome-to-golden-land',
-            'video_url' => 'http://localhost:3000/myanmar.mov',
-            'image_url' => 'http://localhost:3000/myanmar.png',
+            'video_url' => 'https://tourism-mm.vercel.app/myanmar.mov',
+            'image_url' => 'https://tourism-mm.vercel.app/myanmar.png',
             'description' => 'Welcome to Myanmar, the Golden Land. Explore the rich culture, history, and natural beauty of this Southeast Asian gem.',
             'is_active' => true,    
         ]);
