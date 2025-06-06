2cb421768d1760a525c03638708e7716d00144d3
Cisco Ramon <aungkoman@gmail.com>
2025-06-06 07:14:05 +0630

    destination api added

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    //
+    
+    public function index(Request $request)
+    {
+        $data = Destination::all();
+        return response()->json($data);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -162,10 +162,10 @@ Route::get('v1/events/{slug}', function ($slug) {
 });
 
 // get all destinations
-Route::get('v1/destinations', function (Request $request) {
-    $destinations = Destination::all();
-    return response()->json($destinations);
-});
+// Route::get('v1/destinations', function (Request $request) {
+//     $destinations = Destination::all();
+//     return response()->json($destinations);
+// });
 Route::get('v1/articles', function (Request $request) {
     $articles = Article::all();
     return response()->json($articles);
@@ -194,66 +194,6 @@ Route::get('v1/search', function (Request $request) {
 });
 
 
-Route::get('v1/hotels', function (Request $request) {
-    // $hotels = Hotel::all();
-    // return response()->json($hotels);
-    $lat = $request->query('lat');
-    $lng = $request->query('lng');
-
-    if (!$lat || !$lng) {
-        return response()->json(['error' => 'Latitude and longitude are required.'], 400);
-    }
-
-    $hotels = Hotel::select("*", DB::raw("
-        (6371 * acos(
-            cos(radians(?)) *
-            cos(radians(lat)) *
-            cos(radians(lng) - radians(?)) +
-            sin(radians(?)) *
-            sin(radians(lat))
-        )) AS distance
-    "))
-    ->setBindings([$lat, $lng, $lat])
-    ->orderBy('distance')
-    // ->limit(1)
-    ->get();
-
-    return response()->json($hotels);
-});
-
-Route::get('v1/hotels/{id}', function ($id) {
-    $hotel = Hotel::find($id);
-    if ($hotel) {
-        return response()->json($hotel);
-    } else {
-        return response()->json(['message' => 'Hotel not found'], 404);
-    }
-});
-
-Route::post('v1/hotels', function (Request $request) {
-    $hotel = Hotel::create($request->all());
-    return response()->json($hotel, 201);
-});
-
-Route::put('v1/hotels/{id}', function (Request $request, $id) {
-    $hotel = Hotel::find($id);
-    if ($hotel) {
-        $hotel->update($request->all());
-        return response()->json($hotel);
-    } else {
-        return response()->json(['message' => 'Hotel not found'], 404);
-    }
-});
-
-Route::delete('v1/hotels/{id}', function ($id) {
-    $hotel = Hotel::find($id);
-    if ($hotel) {
-        $hotel->delete();
-        return response()->json(['message' => 'Hotel deleted successfully']);
-    } else {
-        return response()->json(['message' => 'Hotel not found'], 404);
-    }
-});
 
 // Route::get('v1/test', function (Request $request) {
 //    return response()->json([
