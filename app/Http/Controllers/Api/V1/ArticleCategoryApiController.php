128d44d7be10b5ff5f670b98740a963c67805363
Cisco Ramon <aungkoman@gmail.com>
2025-06-06 08:12:26 +0630

    Update ArticleCategoryApiController.php

--- app/Http/Controllers/Api/V1/ArticleCategoryApiController.php
+++ app/Http/Controllers/Api/V1/ArticleCategoryApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleCategoryLiteResource;
 use App\Models\Article;
 use App\Models\ArticleCategory;
 use Illuminate\Http\Request;
@@ -11,8 +12,9 @@ class ArticleCategoryApiController extends Controller
 {
     public function index(Request $request)
     {
-        $datas = ArticleCategory::all();
-        return response()->json($datas);
+        $data = ArticleCategory::all();
+        $articleCategoriesData = ArticleCategoryLiteResource::collection($data);
+        return response()->json($articleCategoriesData);
     }
 
     public function show(Request $request, $slug)
